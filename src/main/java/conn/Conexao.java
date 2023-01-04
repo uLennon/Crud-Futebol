f356a03e55e00b1ee223cb6e14210229b39daa73
Lennon <lennonemp@gmail.com>
2023-01-04 13:43:07 -0300

    add conexao

---
+++ src/main/java/conn/Conexao.java
@@ -0,0 +1,20 @@
+package conn;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    public Connection getConnection(){
+
+        String url = "jdbc:mysql://localhost:3306/futt";
+        String username = "root";
+        String password = "root";
+        try{
+            return DriverManager.getConnection(url, username, password);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
